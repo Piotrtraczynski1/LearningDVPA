026adfaec8a1cd1bac10df324a050d8b6c8b6a47
Piotr Traczyński <piotr.traczynski1@gmail.com>
2025-08-31 18:07:30 +0200

    Refactor main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,4 @@
-#include "common/VPA.hpp"
-#include "learner/Learner.hpp"
-// #include "parser/Parser.hpp"
 #include "generator/Tester.hpp"
-#include "teacher/Teacher.hpp"
-#include "utils/TimeMarker.hpp"
-
-std::string findPath()
-{
-    std::string path = std::string(__FILE__);
-    path = path.substr(0, path.find_last_of("/\\") + 1);
-    // path += "parser/inputs/input.json";
-    path += "parser/inputs/input2.json";
-    // path += "parser/inputs/OnlyLocal.json";
-    return path;
-}
 
 int main()
 {
